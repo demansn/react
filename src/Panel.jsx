da56750b0765a4313a045be789c2e2b95fd0c0e7
Dmitriy Lysenko <d.lysenko@lucky-labs.com>
2017-01-20 17:59:11 +0200

    added icons

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -7,8 +7,8 @@ function Panel() {
     return (
         <div className="panel-edit-property" >
             <header className='title'>Спрайт</header>
-            <section>
-                <div><span>visible</span><span><button><i></i></button></span></div>
+            <section className='properties'>
+                <div className='edit-property'><div className='title'>visible</div><div className='property'><button><i className="material-icon">check_box_outline_blank</i></button></div></div>
             </section>
         </div>
     );
